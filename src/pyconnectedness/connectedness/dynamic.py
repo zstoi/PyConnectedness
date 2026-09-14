db6e762ecf7bf0818533c8101e7cb7e8903a09c7
miljana-23 <cvetkovicmiljana93@gmail.com>
2026-09-15 00:35:32 +0200

    reverse T

--- src/pyconnectedness/connectedness/dynamic.py
+++ src/pyconnectedness/connectedness/dynamic.py
@@ -139,7 +139,7 @@ def dynamic_connectedness(
     names = list(data.columns)
 
     pairwise = theta.transpose(0,2,1) - theta # add
-    pairwise = pairwise.T
+    # pairwise = pairwise.T
  
     return DynamicConnectednessResult(
         total=pd.Series(total, index=index, name="total"),
