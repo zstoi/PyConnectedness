6fd53182e2e2f25e60f4802f28334d3db851d952
Zoran Stoiljkovic <zstoilj01@gmail.com>
2026-09-21 22:24:12 +0300

    init upd

--- src/pyconnectedness/connectedness/__init__.py
+++ src/pyconnectedness/connectedness/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .decomposition import generalized_fevd, normalize_fevd, orthogonalized_fevd
 from .dynamic import DynamicConnectednessResult, dynamic_connectedness
 from .static import ConnectednessResult, static_connectedness
+from .frequency import FrequencyConnectednessResult, frequency_connectedness
 from .var import VARFit, fit_var
 
 __all__ = [
@@ -14,7 +15,9 @@ __all__ = [
     "orthogonalized_fevd",
     "normalize_fevd",
     "dynamic_connectedness", 
-    "DynamicConnectednessResult"
+    "DynamicConnectednessResult", 
+    "frequency_connectedness",
+    "FrequencyConnectednessResult", 
 ]
 
 
