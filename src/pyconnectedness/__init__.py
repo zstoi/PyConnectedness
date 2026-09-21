bb83a533b7a58e7d32d6faad8374bd9f4f937596
Zoran Stoiljkovic <zstoilj01@gmail.com>
2026-09-21 22:28:00 +0300

    update init pyconnectedness

--- src/pyconnectedness/__init__.py
+++ src/pyconnectedness/__init__.py
@@ -1,6 +1,5 @@
 """
 Connectedness measures
-VAR wrapper - in progress
 """
 
 from __future__ import annotations
@@ -9,18 +8,21 @@ from .connectedness import (
                             ConnectednessResult,
                             DynamicConnectednessResult,
                             VARFit,
+                            FrequencyConnectednessResult,
                             dynamic_connectedness,
                             fit_var,
                             generalized_fevd,
                             normalize_fevd,
                             orthogonalized_fevd,
-                            static_connectedness,
+                            static_connectedness,   
+                            frequency_connectedness,
 )
 
 __all__ = ["fit_var", "VARFit", 
            "generalized_fevd", "normalize_fevd", "orthogonalized_fevd",
            "ConnectednessResult", "static_connectedness", 
-           "DynamicConnectednessResult", "dynamic_connectedness"
+           "DynamicConnectednessResult", "dynamic_connectedness", 
+           "FrequencyConnectednessResult, frequency_connectedness",
            ] 
 
 
